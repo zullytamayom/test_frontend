909a37f52ddcc42ba1b8d7e744ba5bf4634a8307
zullytamayom <zullytamayom@gmail.com>
2024-04-29 22:57:46 -0500

    se agrega recurso proyecto razor y recurso para traduccion

--- test_frontend/Program.cs
+++ test_frontend/Program.cs
@@ -1,9 +1,19 @@
+using Microsoft.AspNetCore.Mvc.Razor;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages()
+    .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+    .AddDataAnnotationsLocalization();
 
 var app = builder.Build();
+var supportedCultures = new[] { "es", "en" };
+var localizationOptions= new RequestLocalizationOptions().SetDefaultCulture(supportedCultures[0])
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+app.UseRequestLocalization(localizationOptions);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
