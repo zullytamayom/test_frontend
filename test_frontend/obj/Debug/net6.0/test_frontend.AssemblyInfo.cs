f3b7505d931b00185fc46dd25af17dbaa29ce82c
zullytamayom <zullytamayom@gmail.com>
2024-05-03 16:49:28 -0500

    footer

--- test_frontend/obj/Debug/net6.0/test_frontend.AssemblyInfo.cs
+++ test_frontend/obj/Debug/net6.0/test_frontend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test_frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50e44f9426287099107c6b2e9aaf77f9bbaf4ea9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c80282fca2d3a09ba4680ba8c144b5f9cee0138e")]
 [assembly: System.Reflection.AssemblyProductAttribute("test_frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test_frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
